9334afbefd535a48f4e7a83f396b78bbaa1587d7
nielinjie <nielinjie@gmail.com>
2021-09-06 16:09:11 +0800

    more real world testing

--- src/realWorld.test.ts
+++ src/realWorld.test.ts
@@ -1,19 +1,19 @@
-import path from 'path';
-import fs from 'fs-extra';
-import yaml from 'js-yaml';
-import { MapToArrayRule } from './MapToArray';
-import { ChildRule, ShortOnParentRule } from './ShortOnParent';
-import Joi from 'joi';
-import { chain } from './Shorthand';
-import { DotAsNestRule } from './DotAsNest';
+import path from "path";
+import fs from "fs-extra";
+import yaml from "js-yaml";
+import { MapToArrayRule } from "./MapToArray";
+import { ChildRule, ShortOnParentRule } from "./ShortOnParent";
+import Joi from "joi";
+import { chain } from "./Shorthand";
+import { DotAsNestRule } from "./DotAsNest";
 
 test("basic entities", () => {
-  const [obj, resultO] = useTestAndResult("basicEntities",'entities');
- const r = new MapToArrayRule("$.entities", "name");
- const r2 = new MapToArrayRule("$..properties", "name");
- expect(obj).not.toBeNull;
- const re = chain(r,r2).run(obj);
- expect(re[0]).toEqual(resultO);
+  const [obj, resultO] = useTestAndResult("basicEntities", "entities");
+  const r = new MapToArrayRule("$.entities", "name");
+  const r2 = new MapToArrayRule("$..properties", "name");
+  expect(obj).not.toBeNull;
+  const re = chain(r, r2).run(obj);
+  expect(re[0]).toEqual(resultO);
   expect(re[0]).toEqual(resultO);
 });
 test("short on parent entities no constraints", () => {
@@ -23,12 +23,15 @@ test("short on parent entities no constraints", () => {
   );
   const r = new MapToArrayRule("$.entities", "name");
   const r2 = new MapToArrayRule("$..properties", "name");
-  const r3 =  new ShortOnParentRule('$..properties.*',[
-      new ChildRule('type',Joi.string())
-  ],undefined,'_$');
+  const r3 = new ShortOnParentRule(
+    "$..properties.*",
+    [new ChildRule("type", Joi.string())],
+    undefined,
+    "_$"
+  );
 
   expect(obj).not.toBeNull;
-  const re = chain(r,r3,r2).run(obj);
+  const re = chain(r, r3, r2).run(obj);
   expect(re[0]).toEqual(resultO);
   expect(re[0]).toEqual(resultO);
 });
@@ -37,33 +40,52 @@ test("short on parent entities", () => {
     "shortOnParentEntitiesWithConstrains",
     "entities"
   );
-  const r = new MapToArrayRule("$.entities", "name","_$");
-  const r2 = new MapToArrayRule("$..properties", "name","_$");
+  const r = new MapToArrayRule("$.entities", "name", "_$");
+  const r2 = new MapToArrayRule("$..properties", "name", "_$");
   const r3 = new ShortOnParentRule(
     "$..properties[*]",
     [new ChildRule("type", Joi.string())],
     undefined,
     "_$"
   );
-  const r4 =  new DotAsNestRule('$..properties','.','_$')
+  const r4 = new DotAsNestRule("$..properties", ".", "_$");
 
-  expect(obj).not.toBeNull;
-  const re = chain(r,r4,r2,r3).run(obj);
+  const re = chain(r, r4, r2, r3).run(obj);
   // dump(re)
   expect(re[0]).toEqual(resultO);
   // console.log(yaml.dump(resultO))
 });
+test("functions", () => {
+  const [obj, result] = useTestAndResult("functions");
+  const r = new MapToArrayRule("$.functions", "name", "_$");
+  const r2 = new DotAsNestRule("$..annotations", ".", "_$");
+  const re = chain(r, r2).run(obj);
+  expect(re[0]).toEqual(result);
+});
+test("pages", () => {
+  const [obj, result] = useTestAndResult("pages");
+  const r = new MapToArrayRule("$.pages", "name", "_$");
+  const r1 = new MapToArrayRule("$..places", "function", "_$");
+  const r2 = new DotAsNestRule("$.pages[*]", ".", "_$");
+  const re = chain(r,r1, r2).run(obj);
+  expect(re[0]).toEqual(result);
+});
 
-
-function useTestAndResult(name: string,resultName:string|undefined): [object, object] {
+function useTestAndResult(
+  name: string,
+  resultName: string | undefined = undefined
+): [object, object] {
   const file = path.join("./realWorldTestYamls/", `test-${name}.yaml`);
-  const result = path.join("./realWorldTestYamls/", `result-${resultName??name}.yaml`);
+  const result = path.join(
+    "./realWorldTestYamls/",
+    `result-${resultName ?? name}.yaml`
+  );
   const string = fs.readFileSync(file).toString();
   const resultO = yaml.load(fs.readFileSync(result).toString());
   const obj = yaml.load(string);
   return [obj, resultO];
 }
-function dump(re){
-    console.log(yaml.dump(re[0]))
-    console.log(re[1])
+function dump(re) {
+  console.log(yaml.dump(re[0]));
+  console.log(re[1]);
 }
